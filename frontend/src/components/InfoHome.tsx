e874624ff737523151b882d2d3be0f3de29c36d1
Evelyne Arits <evelyne.arits162@gmail.com>
2026-01-08 12:38:48 +0100

    Updated register form to use svg image + no scroll

--- frontend/src/components/InfoHome.tsx
+++ frontend/src/components/InfoHome.tsx
@@ -1,17 +1,15 @@
 import { Link } from "react-router";
+import HomeBackground from "./HomeBackground";
 
 const InfoHome = () => {
   return (
-    <div className="infohome-background">
-      <Link to={"/"}>
-        <img className="infohome-logo" src="/images/logo-clair.png" alt="Logo" />
+    <div className="relative h-full w-full overflow-hidden z-0">
+      <HomeBackground />
+      <Link to={"/"} className="relative z-10">
+        <p className="font-poppins-extra-bold text-white text-4xl">GiftChat.</p>
       </Link>
-      <img className="infohome-serpentin" src="/images/serpentin-jaune.png" alt="Serpentin Jaune" />
-      <img className="infohome-carre" src="/images/carre-vert.png" alt="Carré vert" />
-      <img className="infohome-etoile" src="/images/etoile-rose.png" alt="Etoile rose" />
-      <img className="infohome-cotillon" src="/images/cotillon-rouge.png" alt="Cotillon rouge" />
-      <div className="infohome-content">
-        <h1 className="infohome-content-text">
+      <div className="relative z-10 flex flex-col items-center h-full justify-center ">
+        <h1 className="flex items-center justify-center text-5xl font-inter font-normal text-white leading-[70px] mt-0">
           Faites plaisir à vos proches en leur offrant le cadeau de leurs rêves.
         </h1>
       </div>
@@ -20,3 +18,10 @@ const InfoHome = () => {
 };
 
 export default InfoHome;
+
+{
+  /* <img className="absolute -z-10 w-[600px] top-[10px] left-[-96px]" src="/images/serpentin-jaune.png" alt="Serpentin Jaune" />
+      <img className="absolute -z-10 w-[200px] top-[200px] right-5" src="/images/carre-vert.png" alt="Carré vert" />
+      <img className="absolute -z-10 w-[300px] top-[800px]" src="/images/etoile-rose.png" alt="Etoile rose" />
+      <img className="absolute -z-10 w-[600px] top-[700px] right-[-100px]" src="/images/cotillon-rouge.png" alt="Cotillon rouge" /> */
+}
--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -2,35 +2,60 @@ import RegisterForm from "../components/auth/RegisterForm";
 import InfoHome from "../components/InfoHome";
 import "../components/auth/auth.css";
 
+// const RegisterPage = () => {
+//   return (
+//     <div className="min-h-screen flex flex-col md:flex-row">
+//       <div className="flex-1">
+//         <InfoHome />
+//       </div>
+//       <div className="flex-1 default-background auth-page-mobile-wrapper">
+//         {/* Décorations mobiles - différentes de la page login */}
+//         <img
+//           className="auth-mobile-decoration auth-mobile-serpentin-register"
+//           src="/images/serpentin-jaune.png"
+//           alt="Serpentin Jaune"
+//         />
+//         <img
+//           className="auth-mobile-decoration auth-mobile-carre-register"
+//           src="/images/carre-vert.png"
+//           alt="Carré vert"
+//         />
+//         <img
+//           className="auth-mobile-decoration auth-mobile-etoile-register"
+//           src="/images/etoile-rose.png"
+//           alt="Etoile rose"
+//         />
+//         <img
+//           className="auth-mobile-decoration auth-mobile-cotillon-register"
+//           src="/images/cotillon-rouge.png"
+//           alt="Cotillon rouge"
+//         />
+
+//         <RegisterForm />
+//       </div>
+//     </div>
+
+//     <div className="h-full flex flex-col justify-between">
+//       <div className="w-1/2">
+//         <InfoHome />
+//       </div>
+
+//       <RegisterForm />
+//     </div>
+
+//   );
+// };
+
 const RegisterPage = () => {
   return (
-    <div className="min-h-screen flex flex-col md:flex-row">
-      <div className="flex-1">
+    <div className="flex h-screen w-screen overflow-hidden">
+      {/* Left column */}
+      <div className="w-1/2 h-full">
         <InfoHome />
       </div>
-      <div className="flex-1 default-background auth-page-mobile-wrapper">
-        {/* Décorations mobiles - différentes de la page login */}
-        <img
-          className="auth-mobile-decoration auth-mobile-serpentin-register"
-          src="/images/serpentin-jaune.png"
-          alt="Serpentin Jaune"
-        />
-        <img
-          className="auth-mobile-decoration auth-mobile-carre-register"
-          src="/images/carre-vert.png"
-          alt="Carré vert"
-        />
-        <img
-          className="auth-mobile-decoration auth-mobile-etoile-register"
-          src="/images/etoile-rose.png"
-          alt="Etoile rose"
-        />
-        <img
-          className="auth-mobile-decoration auth-mobile-cotillon-register"
-          src="/images/cotillon-rouge.png"
-          alt="Cotillon rouge"
-        />
 
+      {/* Right column */}
+      <div className="w-1/2 h-full">
         <RegisterForm />
       </div>
     </div>
