a0ba1a0e341de7dca48f39635818725d05030449
ChloeNuage <chloe.bretnacher@gmail.com>
2026-04-03 10:20:40 +0200

    minor css

--- frontend/src/components/utils/Subtitle.tsx
+++ frontend/src/components/utils/Subtitle.tsx
@@ -7,5 +7,11 @@ type SubtitleProps = {
 export default function Subtitle({ dark = false, className, children }: SubtitleProps) {
   const fontColour = dark ? "text-dark" : "text-white";
 
-  return <h3 className={`${fontColour} font-poppins-extra-bold ${className}`}>{children}</h3>;
+  return (
+    <h3
+      className={`${fontColour} ${!className?.includes("font-") ? "font-poppins-extra-bold" : ""} ${className}`}
+    >
+      {children}
+    </h3>
+  );
 }
